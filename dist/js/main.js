c69e1a0c1e0d1bbe418bd6319b608b629178d8f9
madjack <madjack@nm.ru>
2019-04-04 18:50:29 +0400

    add mobile version

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,36 @@
-const carouselTitles = document.querySelector(".carousel-titles").children;
-console.log(carouselTitles);
-const titleElements = Array.from(carouselTitles);
+const titleElements = Array.from(
+  document.querySelector(".carousel-titles").children
+);
+const carouselItems = Array.from(
+  document.querySelector(".carousel-items").children
+);
+
+function getElementName(element) {
+  let specificClassName = element.classList[1];
+  let splitClassName = specificClassName.split("-")[1];
+  return splitClassName;
+}
+
+function findNameInCarouselItems(name) {
+  carouselItems.forEach(carouselItem => {
+    if (carouselItem.classList[0].split("-")[1] == name) {
+      carouselItem.classList.add("visible");
+    } else {
+      carouselItem.classList.remove("visible");
+    }
+  });
+}
+
 titleElements.forEach(element => {
   element.addEventListener("click", () => {
     titleElements.forEach(titleElement => {
       titleElement.classList.remove("active");
     });
     element.classList.add("active");
+    const name = getElementName(element);
+    findNameInCarouselItems(name);
+    console.log(name);
   });
 });
+
+document.querySelector(".a-smart").click();
